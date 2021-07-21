23b9e1948bd8c5db837a6bee4d89b3c2132ec2a7
ruben-ruiz <r.ruiz24@outlook.com>
2021-07-20 21:45:27 -0400

    added /* as a catch all with webpack to render /

--- category-server.js
+++ category-server.js
@@ -11,7 +11,15 @@ app.use(cors());
 const categoryJson = fs.readFileSync('category.json', 'utf8');
 
 app.get('/api/products', (req, res) => {
-    res.send(categoryJson);
+  res.send(categoryJson);
+})
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/public/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
 })
 
 app.listen(port, () => { console.log(`server is running http://${host}:${port}`) });
