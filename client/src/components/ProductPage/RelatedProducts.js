51aee745b4d9c7aa4fe77e786d3cf309d6eb6e64
ruben-ruiz <r.ruiz24@outlook.com>
2021-07-20 21:17:43 -0400

    rendering Overview page accurately with needed data

--- client/src/components/ProductPage/RelatedProducts.js
+++ client/src/components/ProductPage/RelatedProducts.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
-const RelatedProducts = () => {
+const RelatedProducts = ({ styledWith }) => {
   return (
-    <div>RelatedProducts</div>
+    <div className="relatedProducts">
+      {Object.keys(styledWith).length > 0 ?
+      <>
+        <h1>Shop The Look</h1>
+        <div className="relatedProducts-items">
+        {styledWith.map(s =>
+          <img key={s.productCode} src={`https://www.jcrew.com/s7-img-facade/${s.productCode}_${s.defaultColorCode}`} />
+        )}
+      </div>
+      </> :
+      <></>
+      }
+    </div>
   )
 }
 
