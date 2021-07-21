b70abd41fc2488a9ee9a950851da293b5e6054b7
ruben-ruiz <r.ruiz24@outlook.com>
2021-07-20 21:44:51 -0400

    removed SearchCriteria component

--- client/src/components/ProductSearch/Results.js
+++ client/src/components/ProductSearch/Results.js
@@ -4,7 +4,7 @@ import SearchCriteria from './SearchCriteria';
 
 const Results = ({products, navigation, sortBy, shopBy}) => (
   <div className="results">
-    <SearchCriteria navigation={navigation} sortBy={sortBy} shopBy={shopBy} />
+    {/* <SearchCriteria navigation={navigation} sortBy={sortBy} shopBy={shopBy} /> */}
     <Products products={products}/>
   </div>
 )
